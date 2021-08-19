3abd41b512a1851bf5c17f965c3173a8c9e7ca98
Grigorii K. Shartsev <me@shgk.me>
2021-08-19 21:02:09 +0500

    Update eslint config with tests dir

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,7 +49,7 @@ module.exports = {
 
   overrides: [
     {
-      files: ['**/__tests__/*.js?(x)'],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true,
       },
