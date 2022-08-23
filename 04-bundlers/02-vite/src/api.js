89eb0b2ec21b731f9c4c72ec9a2e5bf0a0724376
Grigorii K. Shartsev <me@shgk.me>
2022-08-24 03:14:57 +0500

    04-vue-cli

--- 04-bundlers/02-vite/src/api.js
+++ 04-bundlers/02-vite/src/api.js
@@ -1,5 +1,5 @@
 /** URL адрес API */
-export const API_URL = process.env.VUE_APP_API_URL;
+export const API_URL = import.meta.env.VITE_API_URL;
 
 /**
  * Получает список митапов с API
--- 04-bundlers/02-vite/src/components/PageMeetups.vue
+++ 04-bundlers/02-vite/src/components/PageMeetups.vue
@@ -1,15 +1,15 @@
 <template>
-  <UiContainer>
+  <ui-container>
     <div class="filters-panel">
       <div class="filters-panel__col">
-        <UiRadioGroup v-model="filter.date" :options="$options.dateFilterOptions" name="date" />
+        <ui-radio-group v-model="filter.date" :options="$options.dateFilterOptions" name="date" />
       </div>
 
       <div class="filters-panel__col">
         <div class="form-group form-group_inline">
           <div class="input-group input-group_icon input-group_icon-left">
             <div class="input-group__icon">
-              <UiIcon icon="search" />
+              <ui-icon icon="search" />
             </div>
 
             <input
@@ -21,20 +21,20 @@
           </div>
         </div>
         <div class="form-group form-group_inline">
-          <UiButtonGroup v-model:view="view" />
+          <ui-button-group v-model:view="view" />
         </div>
       </div>
     </div>
 
     <template v-if="meetups">
       <template v-if="filteredMeetups.length">
-        <MeetupsList v-if="view === 'list'" :meetups="filteredMeetups" />
-        <MeetupsCalendar v-else-if="view === 'calendar'" :meetups="filteredMeetups" />
+        <meetups-list v-if="view === 'list'" :meetups="filteredMeetups" />
+        <meetups-calendar v-else-if="view === 'calendar'" :meetups="filteredMeetups" />
       </template>
-      <UiAlert v-else>Митапов по заданным условиям не найдено...</UiAlert>
+      <ui-alert v-else>Митапов по заданным условиям не найдено...</ui-alert>
     </template>
-    <UiAlert v-else>Загрузка...</UiAlert>
-  </UiContainer>
+    <ui-alert v-else>Загрузка...</ui-alert>
+  </ui-container>
 </template>
 
 <script>
@@ -44,8 +44,8 @@ import UiRadioGroup from '@/components/UiRadioGroup.vue';
 import UiButtonGroup from '@/components/UiButtonGroup.vue';
 import UiContainer from '@/components/UiContainer.vue';
 import UiAlert from '@/components/UiAlert.vue';
-import UiIcon from '@/components/UiIcon.vue';
 import { fetchMeetups } from '@/api';
+import UiIcon from './UiIcon.vue';
 
 export default {
   name: 'PageMeetups',
@@ -57,13 +57,13 @@ export default {
   ],
 
   components: {
+    UiIcon,
     MeetupsList,
     MeetupsCalendar,
     UiRadioGroup,
     UiButtonGroup,
     UiContainer,
     UiAlert,
-    UiIcon,
   },
 
   data() {
@@ -82,6 +82,10 @@ export default {
 
   computed: {
     filteredMeetups() {
+      if (!this.meetups) {
+        return null;
+      }
+
       const dateFilter = (meetup) =>
         this.filter.date === 'all' ||
         (this.filter.date === 'past' && new Date(meetup.date) <= new Date()) ||
@@ -106,6 +110,16 @@ export default {
     fetchMeetups().then((meetups) => {
       this.meetups = meetups;
     });
+  },
+
+  methods: {
+    formatDate(date) {
+      return new Date(date).toLocaleString(navigator.language, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      });
+    },
   },
 };
 </script>
